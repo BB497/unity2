75977b589006dddae72e06ab4045d4924c61ffce
BB497 <45710478+BB497@users.noreply.github.com>
2023-03-16 18:50:49 +0900

    day1

---
+++ day1/Classes/Day1/Class1.cs
@@ -0,0 +1,37 @@
+﻿#define E01_01
+#define E01_02
+#define E01_03
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1.Classes.Day1
+{
+	class Class1
+	{
+
+		// 초기화
+		public static void Start(string[] args)
+		{
+#if E01_01
+			class Cplayer
+		{
+			public int m_nLV = 0;
+			public string m_oName = string.Empty;
+
+			public void ShowInfo()
+			{
+				Console.WriteLine("Name: {0}, Lv: {1}", m_oName, m_nLV);
+			}
+		}
+#elif E01_02
+
+#elif E01_03
+
+#endif
+		}
+	}
+}
---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Classes.Day1.Class1.Start(args);
+			Console.ReadKey();
+		}
+	}
+}
