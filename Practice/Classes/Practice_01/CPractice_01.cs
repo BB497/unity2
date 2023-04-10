1e1839138c6f02f89e60719d182bc7a598d918ba
BB497 <tynlove0704@gmail.com>
2023-04-10 17:16:51 +0900

    혼자 연습

---
+++ Practice/Classes/Practice_01/CPractice_01.cs
@@ -0,0 +1,36 @@
+﻿#define P01_01
+//#define P01_02
+//#define P01_03
+//#define P01_04
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Classes.Practice_01 {
+    class CPractice_01 {
+#if P01_01
+#elif P01_02
+#elif P01_03
+#elif P01_04
+#endif  // #if P01_01
+
+        public static void Start(string[] args) {
+#if P01_01
+            var Random = new Random();
+            int Answer = Random.Next(1, 100);
+
+            Console.WriteLine("정답: {0}\n", Answer);
+
+            while (true) {
+                int.TryParse(Console.ReadLine(), out int Val);
+            }
+#elif P01_02
+#elif P01_03
+#elif P01_04
+#endif // #if P01_01
+        }
+    }
+}
