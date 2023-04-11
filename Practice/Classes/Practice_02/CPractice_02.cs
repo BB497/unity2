0ff59d5a477e727b8a3f5e7dc4057d45db98ca82
BB497 <tynlove0704@gmail.com>
2023-04-11 19:49:18 +0900

    asd

--- Practice/Classes/Practice_02/CPractice_02.cs
+++ Practice/Classes/Practice_02/CPractice_02.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿//#define P01_01
+#define P01_02
+//#define P01_03
+//#define P01_04
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Practice.Classes.Practice_02 {
-    internal class CPractice_02 {
+    class CPractice_02 {
+        #if P01_01
+#elif P01_02
+#elif P01_03
+#elif P01_04
+#endif  // #if P01_01
+
+        public static void Start(string[] args) {
+#if P01_01
+#elif P01_02
+#elif P01_03
+#elif P01_04
+#endif // #if P01_01
+        }
     }
 }
